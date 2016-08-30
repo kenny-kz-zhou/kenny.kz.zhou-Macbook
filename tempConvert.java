92e7a17ebf05ad5694bf1705b475c40a9208c4be
kenny-kz-zhou <kenjizzyzhou@Kennys-MacBook-Pro.local>
2016-08-29 21:55:24 -0700

    Fahrenheit to Celsius

---
+++ tempConvert.java
@@ -0,0 +1,20 @@
+package tempConvert;
+
+// Author: Kenny Zhou
+// Name: Farenheit to Celsius
+
+public class tempConvert {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double convFactor = 5/9;
+		int degF = -32;
+		double degC = convFactor * degF;
+		
+		System.out.println(degC);
+		
+
+	}
+
+}
